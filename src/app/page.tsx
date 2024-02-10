884baa422fc1326d87100227d47879012d0b794f
enslimeseth <ens.limes.eth@gmail.com>
2024-02-10 09:01:28 -0500

    Updated title and formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        hello world
+    // Adjusted padding: py-12 for vertical padding, px-24 for horizontal padding
+    <main className="flex min-h-screen flex-col py-12 px-24">
+      <div className="z-10 max-w-5xl w-full flex flex-col items-center justify-center font-mono text-sm lg:flex">
+        <h1 className="text-center text-2xl lg:text-4xl font-bold">The Block Proposer Leaderboard for Farcaster</h1>
       </div>
     </main>
   );
 }
+
